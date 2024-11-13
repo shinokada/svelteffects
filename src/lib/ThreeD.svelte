227363251e85f3939422f35788085c082a97a188
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-13 19:57:26 +0100

    Update Svelte to svelte 5.1.16

--- src/lib/ThreeD.svelte
+++ src/lib/ThreeD.svelte
@@ -10,9 +10,8 @@
     lineHeight?: string;
     fontWeight?: string;
     textShadow?: string;
-    height?: string;
-    width?: string;
     padding?: string;
+    margin?: string;
   }
 
   let { 
@@ -23,8 +22,7 @@
     lineHeight = '1.5', 
     fontWeight = '400', 
     padding = '1rem',
-    height = 'auto',  // Changed to 'auto'
-    width = 'auto',   // Changed to 'auto'
+    margin,
     textShadow = "-0.0075em 0.0075em 0 #fef2f6, 0.005em 0.005em 0 #f6a5c1, 0.01em 0.01em 0 #f7aac4, 0.015em 0.015em #f7aec7, 0.02em 0.02em 0 #f8b3ca, 0.025em 0.025em 0 #f8b7cd, 0.03em 0.03em 0 #f8bcd0, 0.035em 0.035em 0 #f9c0d3", 
     class:className,
     ...restProps 
@@ -35,6 +33,7 @@
   {...restProps}
   class={className}
   style="
+    display: inline-block;
     color: {color};
     background-color: {backgroundColor};
     font-size: {fontSize};
@@ -42,8 +41,7 @@
     font-weight: {fontWeight};
     text-shadow: {textShadow};
     padding: {padding};
-    height: {height};
-    width: {width};
+    margin: {margin};
   "
 >
   {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { ThreeD } from '$lib';
 </script>
 
-<h1><ThreeD fontSize="50px" padding="3rem" class="mySpan">Welcome to your library project</ThreeD></h1>
+<h1><ThreeD fontSize="50px" padding="3rem">Welcome to your library project</ThreeD></h1>
 
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab adipisci deleniti minima fugiat laboriosam, incidunt ut corporis iure error magni animi sit placeat cupiditate nesciunt ex voluptates odit a molestiae.</p>
 
@@ -11,9 +11,5 @@
   h1{
     text-align: center;
     margin-bottom: 150px;
-  }
-  .mySpan {
-    border: 1px solid #fcdde8;
-    margin-bottom: 50px;
   }
 </style>
