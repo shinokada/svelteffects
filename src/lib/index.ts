2162a39e557bcbdca66ae841c7fe9522351ce206
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-15 09:35:32 +0100

    feat: Shade

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as ThreeD } from './ThreeD.svelte';
 export { default as Retro } from './Retro.svelte';
 export { default as Neon } from './Neon.svelte';
 export { default as Neons } from './Neons.svelte';
+export { default as Shade } from './Shade.svelte';
---
+++ src/routes/shade/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Shade } from '$lib';
+</script>
+
+<Shade>
+  HOVER ME
+</Shade>
+
+<Shade 
+  bgColor="#000000"
+  color="#ffffff"
+  height="400px"
+  size={12}
+  width="800px"
+>
+  Custom Text
+</Shade>
